46924d25bd7d297aa1c79d9f6ada79a1c9f34d3a
logesh <logeshleetronics@gmail.com>
2026-04-08 14:33:24 +0530

    checking webhooks

--- python_learning/functionarg.py
+++ python_learning/functionarg.py
@@ -15,5 +15,7 @@ if __name__ == "__main__": #used to mention a condition that if run directly the
        func1()
     elif sys.argv[1] == "f2":
        func2()
+    else:
+       print("default no arg in cmdline\n usage: python script.py arg1 ")
 
 #if run directly __name = main if imported name = script name that is imported 
